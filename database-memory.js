58f427b59b043a61e890cdf40e5c18761896c1b2
MatheusShirakawa <matheus_shirakawa@outlook.com>
2023-09-24 14:09:22 -0300

    :tada: first commit

---
+++ database-memory.js
@@ -0,0 +1,40 @@
+import { randomUUID } from "node:crypto";
+
+export class DatabaseMemory{
+  #videos = new Map();
+
+  list(){
+    console.log(this.#videos.entries())
+    return Array.from(this.#videos.entries()).map((videoArray)=> {
+      const id = videoArray[0]
+      const data = videoArray[1]
+
+      return {
+        id,
+        ...data,
+      }
+    });
+  }
+
+  // Set, Map
+
+  create(video){
+    const videoId = randomUUID()
+    console.log(videoId)
+    console.log(randomUUID())
+
+    // UUID - unic universe id
+
+    this.#videos.set(videoId, video);
+  }
+
+  update(id, video){
+    this.#videos.set(id, video);
+  }
+
+  delete(id, video){
+    this.#videos.delete(id);
+  }
+
+
+}
---
+++ server.js
@@ -0,0 +1,75 @@
+// import { createServer } from 'node:http'
+
+// const server = createServer((request, response) => {
+//   response.write('Hello World')
+
+//   return response.end()
+// })
+
+// server.listen(3333)
+
+// // POST localhost:3333/videos
+// // DELETE localhost:3333/videos/1
+// // postgree
+// // fastify
+
+import { fastify } from "fastify"
+import { DatabaseMemory } from './database-memory.js'
+const server = fastify()
+const database = new DatabaseMemory()
+// GET , POST, PUT, DELETE, PATCH,
+
+// post http://localhost:3333/videos
+// put http://localhost:3333/videos/3
+
+// Route parameter
+
+// REQUEST BODY
+
+server.post('/videos', (request, reply) => {
+  const {title, description, duration} = request.body
+
+  database.create({
+    title,
+    description,
+    duration
+  })
+
+  console.log(title)
+
+  return reply.status(201).send()
+})
+
+server.get('/videos', () => {
+    const videos = database.list()
+    console.log(videos)
+    return videos
+})
+
+server.put('/videos/:id', (request,reply) => {
+  const videoId = request.params.id
+  const {title, description, duration} = request.body
+
+  database.update(videoId, {
+    title,
+    description,
+    duration
+  })
+
+  return reply.status(204).send()
+})
+
+server.delete('/videos/:id', () => {
+  return 'Hello Node'
+})
+
+server.listen({
+  port:3333,
+})
+
+
+
+
+
+
+
